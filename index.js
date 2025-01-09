532f3218b10a3042865c985de78a3777191e9b5e
rojoriz6 <rjjohnson67@outlook.com>
2025-01-09 18:47:59 -0500

    completed

---
+++ index.js
@@ -0,0 +1,9 @@
+/*Create a string for the user saying: You have received this message because you have been chosen to open an important vault.
+ */
+
+const first= 25 - 15
+const second= 20 * 2
+const third= 26 + 13
+const combination= `You have received this message because you have been chosen to open an important vault. Here is the secret combination: ${first} ${second} ${third}`
+
+alert(combination);
